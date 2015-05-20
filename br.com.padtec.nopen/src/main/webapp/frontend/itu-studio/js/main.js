d4044c1160444d1df8ca83b9008800a28f0e58a1
missaeldenadai <missaeldenadai@users.noreply.github.com>
2015-05-20 13:29:13 -0300

    Remoção do atributo "snapLink" do paper. Se deixar, as conexões param de funcionar

--- br.com.padtec.nopen/src/main/webapp/frontend/itu-studio/js/main.js
+++ br.com.padtec.nopen/src/main/webapp/frontend/itu-studio/js/main.js
@@ -76,9 +76,6 @@ var Rappid = Backbone.Router.extend({
 			// RF: Ao selecionar uma porta, destacar portas disponíveis para conexão com aquela
 			markAvailable: true,
 			
-			// RF: Inserir 'snap link' às conexões
-			snapLinks: { radius: 50 },
-			
             defaultLink: new joint.dia.Link({
                 attrs: {
                     // @TODO: scale(0) fails in Firefox
